a403910950de2803a5b58c53aa6ea62fe3bfa34a
mariebernf <77eyo7ks@students.codeinstitute.net>
2025-05-09 18:19:53 +0200

    Update URL patterns

--- smoothies/urls.py
+++ smoothies/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.smoothie_list, name='smoothie_list'),
     path('create/', views.smoothie_create, name='smoothie_create'),
-    path('update/<int:pk/', views.smoothie_update, name='smoothie_update'),
-    path('delete/<int:pk/', views.smoothie_delete, name='smoothie_delete'), 
+    path('update/<int:pk>/', views.smoothie_update, name='smoothie_update'),
+    path('delete/<int:pk>/', views.smoothie_delete, name='smoothie_delete'),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
